4d40d7dcb612e12d3f45680c394cf240fc54288f
oskayote <oskayote@gmail.com>
2026-03-22 19:45:04 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CryptoDashboard from './CryptoDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CryptoDashboard />
+  </React.StrictMode>
+)
